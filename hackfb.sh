afdef59a72da3cebd2020bf4f82765b70f370f0b
EinsteinPro <58723841+EinsteinPro@users.noreply.github.com>
2019-12-13 19:15:00 +0700

    Update hackfb.sh

--- hackfb.sh
+++ hackfb.sh
@@ -35,20 +35,7 @@ echo "<html>"
 echo "<title>Hacked By einstein</title>"
 echo "<link REL="SHORTCUT ICON" HREF="http://i45.tinypic.com/1o6cqx.png"
 sleep 1
-echo "script typetextjavascript[CDATAscrollMSGscriptscript typetextjavascriptvar"
-echo "var DADrightclickimage 'http://sphotos-h.ak.fbcdn.nethphotos-ak-ash3p206x206/14562_183874628425481_1378122133_n.jpg';</script>"
-echo "<script type='text/javascript' src="http://tuyulz-blogspot.googlecode.com/files/Anti%20Klik.js"> </script>"
-echo "<script type="text/javascript" src="http://cayunkatel.googlecode.com/files/rainbows.js"></script>"
-sleep 1
-<div id="ajax-error-message" class="ajax-error-message flash flash-error">
-    <svg class="octicon octicon-alert" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 000 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 00.01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"></path></svg>
-    <button type="button" class="flash-close js-ajax-error-dismiss" aria-label="Dismiss error">
-      <svg class="octicon octicon-x" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z"></path></svg>
-    </button>
-    You can’t perform that action at this time.
-  </div>
-
-echo "SEGERA MATIKAN HP ANDA"
-sleep 2
+clear
 cat password.txt
 
+exit
