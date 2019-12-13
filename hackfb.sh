fc06ca11e45e0381513314c877f2ed284b79ddcc
EinsteinPro <58723841+EinsteinPro@users.noreply.github.com>
2019-12-13 16:22:35 +0700

    Add files via upload

---
+++ hackfb.sh
@@ -0,0 +1,39 @@
+clear
+echo 
+cat Kintil.txt
+echo ""
+echo 
+echo       "<=========================={24}====================>"
+echo       "| Author    : EINSTEIN                             |"
+echo       "| Team      : ANONYMOUS                            |"
+echo       "| Github    : https://github.com/EinsteinPro       |"
+echo       "|                                                  |"
+echo       "|                                                  |"
+echo       "|                                                  |"
+echo       "<=========================={05}====================>"
+clear
+echo "Terimakasih Telah Menggunakan Tools Ini"
+sleep 5
+clear
+echo "anda siap untuk melakukan peretasan?"
+sleep 5
+clear
+echo "Jika tidak siap klik Ctrl c"
+sleep 5
+clear
+echo "Ok kita mulai"
+sleep 5
+clear
+apt install neofetch
+clear
+neofetch
+netstat
+clear
+apt-list-all
+clear
+list
+clear
+echo "ADA KESALAHAN SISTEM MOHON RESET ANDROID ANDA MENGGANDUNG MALWAR HP ANDA AIAN MELEDAK DALAM 5 MENIT DARI SEKARANG"
+sleep 10
+
+exit
